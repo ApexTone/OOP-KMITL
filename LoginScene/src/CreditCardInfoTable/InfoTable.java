486af623dbe6d5262936c0abe137e62fae5de7b4
Tanapol <korn_tanapol@outlook.co.th>
2020-04-05 16:13:54 +0700

    Delete commenting

--- LoginScene/src/CreditCardInfoTable/InfoTable.java
+++ LoginScene/src/CreditCardInfoTable/InfoTable.java
@@ -29,25 +29,7 @@ public class InfoTable extends Application {
     public void start(Stage stage) throws Exception {
         stage.setTitle("InfoLists");
         stage.setResizable(false);
-        /* //Table don't seems to fit -> use ListView instead
-        Label tableNameLabel = new Label("Credit card info");
-        
-        tableNameLabel.setTextAlignment(TextAlignment.CENTER);
-        
-        TableView infoTable = new TableView();
-        TableColumn listColumn = new TableColumn("List");
-        listColumn.setMinWidth(MAX_WIDTH/2);
-        
-        TableColumn valueColumn = new TableColumn("Value");
-        valueColumn.setMinWidth(MAX_WIDTH/2);
-        
-        infoTable.getColumns().addAll(listColumn,valueColumn);
-        
-        VBox vBox = new VBox();
-        vBox.getChildren().addAll(tableNameLabel,infoTable);
-        Scene scene = new Scene(vBox,MAX_WIDTH,500);
-        stage.setScene(scene);
-         */
+        //Using ListView here instead
         stage.show();
     }
 
