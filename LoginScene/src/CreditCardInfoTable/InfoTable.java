7229c76b2926ea329301a96e6161a95570511b19
Tanapol <korn_tanapol@outlook.co.th>
2020-04-07 15:34:57 +0700

    File reading

--- LoginScene/src/CreditCardInfoTable/InfoTable.java
+++ LoginScene/src/CreditCardInfoTable/InfoTable.java
@@ -5,6 +5,13 @@
  */
 package CreditCardInfoTable;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -34,7 +41,22 @@ public class InfoTable extends Application {
         nameList.getItems().addAll("Serial","Name","Expire Date","Company","Type","Level","Money","State","Start Date");
         
         ListView valueList = new ListView();
-        valueList.getItems().addAll("123456789012","Tanapol Wong-asa","04/20",CreditCard.AEON,"Credit",CreditCard.SILVER,"100000","OK","05/15");
+        //read from text file
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("CardInfoDatabase.txt"));
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                String[] data = line.split(",");//split at ','
+                //System.out.println(Arrays.toString(data));
+                for(int i=0;i<data.length;i++){
+                    valueList.getItems().add(data[i]);
+                }
+            }
+        } catch (FileNotFoundException ex) {
+            System.out.println("Username Database can't be found");
+        } catch (IOException ex) {
+            System.out.println("IO exception smt");
+        }   
 
         HBox hBox = new HBox();
         hBox.setAlignment(Pos.CENTER);
@@ -49,7 +71,50 @@ public class InfoTable extends Application {
         stage.setScene(scene);
         stage.show();
     }
+    
+    public static void createMockDatabase() {//creat mock database
+        File file = new File("CardInfoDatabase.txt");
+        System.out.println(file.exists());
+        char splitter = ',';
+        try {
+            PrintWriter output = new PrintWriter(file);//init PrintWriter (File writer)
+            output.print("123456789012");
+            output.print(splitter);
+            output.print("John Cena");
+            output.print(splitter);
+            output.print("04/20");
+            output.print(splitter);
+            output.print("Aeon");
+            output.print(splitter);
+            output.print("Credit");
+            output.print(splitter);
+            output.print("Gold");
+            output.print(splitter);
+            output.print("100000");
+            output.print(splitter);
+            output.print("OK");
+            output.print(splitter);
+            output.print("05/15");
+            output.close();//Close file to "save" and for "safety"
+        } catch (FileNotFoundException ex) {
+            System.out.println("Username Database can't be found/create");
+        }
+    }
 
+    public static void testCSV() {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("CardInfoDatabase.txt"));
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                String[] data = line.split(",");//split at ','
+                System.out.println(Arrays.toString(data));
+            }
+        } catch (FileNotFoundException ex) {
+            System.out.println("Username Database can't be found");
+        } catch (IOException ex) {
+            System.out.println("IO exception smt");
+        }
+    }
     public static void main(String[] args) {
         launch(args);
     }
